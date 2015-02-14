f8f0f1ba3155f896186575934ba0497358c019d0
mikgol <m.golkowski@live.com.au>
2015-02-14 12:02:41 +1100

    changed mac address

--- www/js/index.js
+++ www/js/index.js
@@ -7,7 +7,7 @@
 
 
 var app = {
-    macAddress: "AA:BB:CC:DD:EE:FF",  // get your mac address from bluetoothSerial.list
+    macAddress: "00:00:12:09:32:96",  // get your mac address from bluetoothSerial.list
     chars: "",
 
 /*
